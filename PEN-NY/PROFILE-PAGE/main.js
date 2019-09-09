d1470480405d274d9ad50c1d186ec2ff028e1e52
rosendopili <rosendopili@gmail.com>
2019-09-09 15:57:49 -0400

    made some changes to the profile page.  Added the API schema to homepage/main.js

--- PEN-NY/PROFILE-PAGE/main.js
+++ PEN-NY/PROFILE-PAGE/main.js
@@ -16,4 +16,46 @@ const getWeather = function() {
   })
 }
 
-console.log("hello world");
+console.log('hello, world');
+//
+// function getData() {
+//     fetch('http://thesi.generalassemb.ly:8080/')
+//     .then((res) => {
+//         console.log(res);
+//         return res.json();
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// }
+
+// function postData() {
+//     fetch('http://thesi.generalassemb.ly:8080/signup', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 email: "BLAHaaaaaaaaa@BLAH.COM",
+//                 password: "12345678aa",
+//                 username: "BLAH aaaaaaaaBLAH"
+//             })
+//     })
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((res) => {
+//         updateDom(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// }
+//
+// function updateDom(data) {
+//     console.log(data.token);
+//     const text = document.querySelector('p');
+//     text.innerText = data.token;
+// }
+//
+// postData()
