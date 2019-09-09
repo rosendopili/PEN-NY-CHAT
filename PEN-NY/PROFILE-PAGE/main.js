1d21f16a1741de549c6ec057a59f4666ec81905c
Rosendo R Pili <54370064+rosendopili@users.noreply.github.com>
2019-09-09 14:57:23 -0400

    STARTER FOLDER

---
+++ PEN-NY/PROFILE-PAGE/main.js
@@ -0,0 +1,17 @@
+//add adorable avatars API: https://api.adorable.io/avatars/223/abott@adorable.png
+
+const getWeather = function() {
+
+  fetch('https://api.adorable.io/avatars/223/abott@adorable.png')
+
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+      getData(res.results[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+  })
+}
