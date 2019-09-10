02b906d25d81b61b2f6a5973645e4a2ed36d022d
srichards567 <srichards567@gmail.com>
2019-09-10 14:38:38 -0400

    Made changes to make clicking on the sign up botton on LANDING-PAGE navigate to PROFILE-PAGE.

--- PEN-NY/LANDING-PAGE/main.js
+++ PEN-NY/LANDING-PAGE/main.js
@@ -1,14 +1,3 @@
-const emailInputField = document.getElementById('email')
-
-emailInputField.addEventListener('click', navigateToProfilePage);
-
-function navigateToProfilePage () {
-  location.replace("../PROFILE-PAGE/index.html")
-}
-
-// if (emailInputField.value.length === 0) {
-//   document.getElementById
-// }
 
 // content window animate
 const window = document.getElementById('idWindow');
