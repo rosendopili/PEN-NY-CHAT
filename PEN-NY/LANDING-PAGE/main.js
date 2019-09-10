11333f9fc9bbe7a7a4f75be131c0147638656988
rosendopili <rosendopili@gmail.com>
2019-09-10 16:41:13 -0400

    this is a test commit

--- PEN-NY/LANDING-PAGE/main.js
+++ PEN-NY/LANDING-PAGE/main.js
@@ -1,8 +1,8 @@
 
 // content window animate
-const window = document.getElementById('idWindow');
-window.onclick = function(event) {
-    if (event.target == window) {
-        window.style.display = "none";
+const modal = document.getElementById('idWindow');
+modal.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
--- PEN-NY/PROFILE-PAGE/main.js
+++ PEN-NY/PROFILE-PAGE/main.js
@@ -1,90 +1,86 @@
-//add adorable avatars API: https://api.adorable.io/avatars/223/abott@adorable.png
-//
-//   fetch('https://api.adorable.io/avatars/223/abott@adorable.png')
-//
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((res) => {
-//       console.log(res);
-//       getData(res.results[0]);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//   })
-// }
-
-console.log('hello, world');
 
 
-function createUser(event) {
-  event.preventDefault();
-  const username = document.getElementById('userName');
-  const email =
-  document.getElementById('email');
-  const password = document.getElementById('password');
+function postData(event) {
+    event.preventDefault();
+    const email = document.querySelector('.email');
+    const password = document.querySelector('.password');
+    const username = document.querySelector('.username');
 
-  fetch('http://thesi.generalassemb.ly:8080/signup', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-              email: email.value,
-              password: password.value,
-              username: userName.value
-          })
-        })
-        .then((res) => {
-            return res.json();
-        })
-        .then((res) => {
-            localStorage.setItem('user', res.token);
-            createPost();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+    fetch('http://thesi.generalassemb.ly:8080/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value,
+                username: username.value
+            })
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        localStorage.setItem('user', res.token);
+        createPost();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 }
-//
-// function getData() {
-//     fetch('http://thesi.generalassemb.ly:8080/')
-//     .then((res) => {
-//         console.log(res);
-//         return res.json();
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     })
-// }
 
-// function postData() {
-//     fetch('http://thesi.generalassemb.ly:8080/signup', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify({
-//                 email: "BLAHaaaaaaaaa@BLAH.COM",
-//                 password: "12345678aa",
-//                 username: "BLAH aaaaaaaaBLAH"
-//             })
-//     })
-//     .then((res) => {
-//         return res.json();
-//     })
-//     .then((res) => {
-//         updateDom(res);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     })
-// }
-//
-// function updateDom(data) {
-//     console.log(data.token);
-//     const text = document.querySelector('p');
-//     text.innerText = data.token;
-// }
-//
-// postData()
+function updateDom() {
+    document.querySelector('.signupForm').style.display = "none";
+    document.querySelector('.postForm').style.display = "block";
+
+    fetch("http://thesi.generalassemb.ly:8080/user/post", {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('user')
+        }
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        const list = document.querySelector('.posts');
+
+        for (let i = 0; i < res.length; i++) {
+            const item = document.createElement('li');
+            const title = document.createElement('h3');
+            const description = document.createElement('p');
+            item.appendChild(title);
+            item.appendChild(description);
+            title.innerText = res[i].title;
+            description.innerText = res[i].description;
+            list.appendChild(item);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+function createPost(event) {
+    event.preventDefault();
+    const title = document.querySelector('.title');
+    const description = document.querySelector('.description');
+
+    fetch("http://thesi.generalassemb.ly:8080/post", {
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('user'),
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title.value,
+            description: description.value
+        })
+    })
+    .then((res) => {
+        console.log(res);
+        updateDom(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
